5528d494cf2b192b08ecf7b6f58d3b24964503e0
walterwenzel <144962219+walterwenzel@users.noreply.github.com>
2023-12-04 01:17:04 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.getElementById("header").innerHTML=` 
     <nav class="navbar navbar-expand-sm navbar-light bg-light">
         <div class="container">        
-            <a class="navbar-brand" href="../templates/productos.html">CRUD</a>
+            <a class="navbar-brand" href="./productos.html">CRUD</a>
         </div>
     </nav>`
